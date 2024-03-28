1a0bf526282fe0cdadc4583bd8ed3e6a6b122b17
Tamilselvan Radhakrishnan <tamilonn@gmail.com>
2024-03-29 01:17:53 +0530

    comment main_bck up file

--- main_bck.py
+++ main_bck.py
@@ -1,49 +1,37 @@
-from fastapi import Depends, FastAPI
-from contextlib import asynccontextmanager
+# from fastapi import Depends, FastAPI
+# from contextlib import asynccontextmanager
 
-#--- Below section not needed here
-# import sys
-# import os
-# myDir = os.getcwd()
-# sys.path.append(myDir)
-
-# from pathlib import Path
-# path = Path(myDir)
-# a=str(path.parent.absolute())
-# sys.path.append(a)
-#--- Above section not needed ---
-
-from app.routers  import svc_routers_v1
+# from app.routers  import svc_routers_v1
 
 
 
 
 
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    # Load startup logic here...
-    try:
-        print('Before starting application....')
-        yield
-    finally:
-    # Shut down logic here....
-        print('After application is shut down....')
+# @asynccontextmanager
+# async def lifespan(app: FastAPI):
+#     # Load startup logic here...
+#     try:
+#         print('Before starting application....')
+#         yield
+#     finally:
+#     # Shut down logic here....
+#         print('After application is shut down....')
 
 
-def create_app():
+# def create_app():
 
-    app = FastAPI(lifespan=lifespan)
+#     app = FastAPI(lifespan=lifespan)
 
-    #v1 routner mappings..
-    app.include_router(svc_routers_v1.router)
-    app.include_router(svc_routers_v1.router, prefix="/v1")
+#     #v1 routner mappings..
+#     app.include_router(svc_routers_v1.router)
+#     app.include_router(svc_routers_v1.router, prefix="/v1")
 
-    return app
+#     return app
 
-app= create_app()
+# app= create_app()
 
 
-@app.get("/")
-async def root():
-    return {"message": "This is py-boilerplate-service"}
+# @app.get("/")
+# async def root():
+#     return {"message": "This is py-boilerplate-service"}
