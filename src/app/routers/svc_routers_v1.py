d41d12e01d8248fbfb296a4c58cc7e6544e71869
Tamilselvan Radhakrishnan <tamilonn@gmail.com>
2024-04-20 14:56:14 +0530

    initial creation of jwt-config-service

--- src/app/routers/svc_routers_v1.py
+++ src/app/routers/svc_routers_v1.py
@@ -3,7 +3,7 @@ from app.models.core.response import ServiceResponse
 from app.models.core.error_response import ErrorResponse
 
 router = APIRouter(
-    prefix="/persons",
+    prefix="/config",
     tags=["persons"],
     responses={404: {"description": "Requested endpoint not available"}},
 )
@@ -19,4 +19,11 @@ async def health():
 async def error():
     error= ErrorResponse(403, 'Service failed!!', None)
     service_response= ServiceResponse(False, '1.0',None, error)
+    return service_response
+
+
+@router.get("/create")
+async def create():
+    create = {"I'm in /create endpoint!!"}
+    service_response= ServiceResponse(True, '1.0',create, None)
     return service_response
